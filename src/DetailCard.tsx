45a6d9aa981e0a56d9561ffb66ce6e4a7393ae9d
Wayne Lee <waynel1996weiyang@gmail.com>
2021-06-12 11:19:31 -0700

    Delete default thing made by create-react-app and add a header and a card

---
+++ src/DetailCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Card, CardHeader} from '@material-ui/core'
+
+export default function DetailCard() {
+  return (
+    <Card>
+      <CardHeader
+        title={"Detail"}
+      />
+    </Card>
+  )
+}
