3d671978dd406af37ba3ad737ea356f96493ca90
Wayne Lee <waynel1996weiyang@gmail.com>
2021-06-12 11:30:07 -0700

    Add some content to card, with minor styling

--- src/DetailCard.tsx
+++ src/DetailCard.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import {Card, CardHeader} from '@material-ui/core'
+import {Card, CardContent, CardHeader, Typography} from '@material-ui/core'
+import {makeStyles, createStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => 
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }
+  })
+)
 
 export default function DetailCard() {
+  const style = useStyles()
+
   return (
-    <Card>
+    <Card className={style.root}>
       <CardHeader
         title={"Detail"}
       />
+      <CardContent>
+        <Typography>
+          Some content
+        </Typography>
+      </CardContent>
     </Card>
   )
 }
